0cc2a43c5d029fc750403ee693bb7efa19d90988
Andy <andy@hydrogenplatform.com>
2018-11-05 15:12:32 -0500

    Team member cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import Profile from './profiles/profile';
+
+library.add(fab)
 
 const styles = {
   height: "100vh",
@@ -12,6 +17,24 @@ class App extends Component {
     return (
       <div style={styles}>
         <span>Project Hydro</span>
+        <span>Team Members</span>
+        <Profile
+          name = "Andy Chorlian"
+          linkedIn="https://www.linkedin.com/in/achorlia/"
+          email="andy@hydrogenplatform.com"
+          twitter="https://twitter.com/andy8052"
+          github="https://github.com/AndyHydro"
+          photoName = "andy.jpeg"
+          background = "architecture.jpg"
+        />
+        <Profile
+          name = "Noah Zinsmeister"
+          linkedIn="https://www.linkedin.com/in/noahzinsmeister/"
+          email="noah@hydrogenplatform.com"
+          twitter="https://twitter.com/noahzinsmeister"
+          github="https://github.com/noahhydro"
+          photoName = "noah.png"
+        />
       </div>
     );
   }
