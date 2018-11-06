11ff9ff28650a0286c67f84be53cb24e93f92afa
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-11-06 10:47:12 -0500

    clean up for public release

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import Profile from './profiles/profile';
-import Footer from './footer';
+import Profile from './profiles/Profile';
+import Footer from './Footer';
 import './sections.css';
-import './header.css';
 
 library.add(fab)
 
@@ -15,11 +14,15 @@ const mainStyles = {
   padding: 0,
   display: "block"
 }
+
 const headerStyles = {
   width: '100%',
   margin: 0,
   padding: 0,
-  display: "flex"
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexWrap: "wrap"
 }
 
 
@@ -27,17 +30,15 @@ class App extends Component {
   render() {
     return (
       <div style={mainStyles}>
-        <header class="header" style={headerStyles}>
-          <h2>Project Hydro Header</h2>
-          <p>have some dope graphic here</p>
-        </header>
-
         <section class="sections">
-          <h2>About project hydro</h2>
+          <h2>Project Hydro Header</h2>
         </section>
 
         <section class="sections">
-          <h2>Team Members</h2>
+          <h2>About Project Hydro</h2>
+        </section>
+
+        <section class="sections">
           <div style={headerStyles} id="cards">
             <Profile
               name = "Andy Chorlian"
@@ -46,7 +47,7 @@ class App extends Component {
               twitter="https://twitter.com/andy8052"
               github="https://github.com/AndyHydro"
               photoName = "andy.jpeg"
-              background = "architecture.jpg"
+              backgroundName = "architecture.jpg"
             />
             <Profile
               name = "Noah Zinsmeister"
@@ -55,6 +56,7 @@ class App extends Component {
               twitter="https://twitter.com/noahzinsmeister"
               github="https://github.com/noahhydro"
               photoName = "noah.png"
+              backgroundName = "stars.png"
             />
             <Profile
               name = "Shane Hampton"
@@ -63,7 +65,7 @@ class App extends Component {
               twitter="https://twitter.com/citizenshane_"
               github="https://github.com/shanehampton"
               photoName = "shane.png"
-              background = "hexagons.png"
+              backgroundName = "hexagons.png"
             />
           </div>
         </section>
