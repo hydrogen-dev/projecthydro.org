872c316d7655e3fb8bd1d640a94603541b847351
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-11-06 10:58:09 -0500

    Updates

---
+++ precache-manifest.3efcce6b37520151b219a71057ef4397.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3079dd2f03a0a752b386",
+    "url": "/static/js/main.3079dd2f.chunk.js"
+  },
+  {
+    "revision": "09e6b101eb818ca88b40",
+    "url": "/static/js/1.09e6b101.chunk.js"
+  },
+  {
+    "revision": "3079dd2f03a0a752b386",
+    "url": "/static/css/main.f2fc1519.chunk.css"
+  },
+  {
+    "revision": "5008d4539d0dc44182a681ec37c0e8d0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/project-hydro-website/precache-manifest.9cc68c353e52ad6690e02910af467da8.js"
+  "/precache-manifest.3efcce6b37520151b219a71057ef4397.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/project-hydro-website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
