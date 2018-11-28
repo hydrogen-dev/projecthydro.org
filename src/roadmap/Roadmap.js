03d237171370c391734abf592335912cc62724cb
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-11-28 17:20:13 -0500

    update travis settings, fix errors

--- src/roadmap/Roadmap.js
+++ src/roadmap/Roadmap.js
@@ -18,18 +18,18 @@ class Roadmap extends Component {
 
   render() {
     return (
-      <div class="roadmap-container">
-        <div class="cardContainer inactive">
-          <div class="roadmap-card">
-            <div class="side front">
-              <div class="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/raindrop.png")`}}></div>
-              <div class="info">
+      <div className="roadmap-container">
+        <div className="cardContainer inactive">
+          <div className="roadmap-card">
+            <div className="side front">
+              <div className="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/raindrop.png")`}}></div>
+              <div className="info">
                 <h2>Raindrop</h2>
                 <p>Two-factor authentication (2FA) on the blockchain.</p>
               </div>
             </div>
-            <div class="side back">
-              <div class="info">
+            <div className="side back">
+              <div className="info">
                 <h2>At a glance</h2>
                 <ul>
                   <li>Protect your account with an Ethereum address</li>
@@ -39,24 +39,24 @@ class Roadmap extends Component {
               </div>
             </div>
           </div>
-          <div class="btn" >
+          <div className="btn" >
             <a href="https://github.com/hydrogen-dev/hydro-docs/blob/master/Raindrop/Raindrop.md">
               <button>Whitepaper</button>
             </a>
           </div>
         </div>
 
-        <div class="cardContainer inactive">
-          <div class="roadmap-card">
-            <div class="side front">
-              <div class="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/snowflake.png")`}}></div>
-              <div class="info">
+        <div className="cardContainer inactive">
+          <div className="roadmap-card">
+            <div className="side front">
+              <div className="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/snowflake.png")`}}></div>
+              <div className="info">
                 <h2>Snowflake</h2>
                 <p>Unique and distributed user identity.</p>
               </div>
             </div>
-            <div class="side back">
-              <div class="info">
+            <div className="side back">
+              <div className="info">
                 <h2>At a glance</h2>
                 <ul>
                   <li>Build on ERC-1484</li>
@@ -67,24 +67,24 @@ class Roadmap extends Component {
               </div>
             </div>
           </div>
-          <div class="btn">
+          <div className="btn">
             <a href="https://github.com/hydrogen-dev/hydro-docs/blob/master/Snowflake/Snowflake_DRAFT.md">
               <button>Whitepaper</button>
             </a>
           </div>
         </div>
 
-        <div class="cardContainer inactive">
-          <div class="roadmap-card">
-            <div class="side front">
-              <div class="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/ice.png")`}}></div>
-              <div class="info">
+        <div className="cardContainer inactive">
+          <div className="roadmap-card">
+            <div className="side front">
+              <div className="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/ice.png")`}}></div>
+              <div className="info">
                 <h2>Ice</h2>
                 <p>Document storage and signing.</p>
               </div>
             </div>
-            <div class="side back">
-              <div class="info">
+            <div className="side back">
+              <div className="info">
                 <h2>At a glance</h2>
                 <ul>
                   <li>Securely sign and stamp documents</li>
@@ -94,24 +94,24 @@ class Roadmap extends Component {
               </div>
             </div>
           </div>
-          <div class="btn">
+          <div className="btn">
             <a href="https://github.com/hydrogen-dev/hydro-docs/blob/master/Ice/Ice_DRAFT.md">
               <button>Whitepaper</button>
             </a>
           </div>
         </div>
 
-        <div class="cardContainer inactive">
-          <div class="roadmap-card">
-            <div class="side front">
-              <div class="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/tide.png")`}}></div>
-              <div class="info">
+        <div className="cardContainer inactive">
+          <div className="roadmap-card">
+            <div className="side front">
+              <div className="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/tide.png")`}}></div>
+              <div className="info">
                 <h2>Tide</h2>
                 <p>A layer 2 payments protocol.</p>
               </div>
             </div>
-            <div class="side back">
-              <div class="info">
+            <div className="side back">
+              <div className="info">
                 <h2>At a glance</h2>
                 <ul>
                   <li>Transact quickly and securely on top of Ethereum</li>
@@ -121,24 +121,24 @@ class Roadmap extends Component {
               </div>
             </div>
           </div>
-          <div class="btn">
+          <div className="btn">
             <a href="#roadmap">
               <button>Coming Soon</button>
             </a>
           </div>
         </div>
 
-        <div class="cardContainer inactive">
-          <div class="roadmap-card">
-            <div class="side front">
-              <div class="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/mist.png")`}}></div>
-              <div class="info">
+        <div className="cardContainer inactive">
+          <div className="roadmap-card">
+            <div className="side front">
+              <div className="img" style={{backgroundImage: `url("${process.env.PUBLIC_URL}/roadmap/mist.png")`}}></div>
+              <div className="info">
                 <h2>Mist</h2>
                 <p>Blockchain based AI.</p>
               </div>
             </div>
-            <div class="side back">
-              <div class="info">
+            <div className="side back">
+              <div className="info">
                 <h2>At a glance</h2>
                 <ul>
                   <li>TODO</li>
@@ -148,7 +148,7 @@ class Roadmap extends Component {
               </div>
             </div>
           </div>
-          <div class="btn">
+          <div className="btn">
             <a href="#roadmap">
               <button>Coming Soon</button>
             </a>
